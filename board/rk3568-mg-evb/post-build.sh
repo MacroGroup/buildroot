ca9ec3610f4cd95689e731e2242cd21d1a740219
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-08-01 17:18:56 +0300

    - Add bash - Restore sound volumes at startup

--- board/rk3568-mg-evb/post-build.sh
+++ board/rk3568-mg-evb/post-build.sh
@@ -2,3 +2,6 @@
 BOARD_DIR="$(dirname $0)"
 
 cp -f $BOARD_DIR/barebox-rk3568-mg-evb.img $BINARIES_DIR
+
+install -m 0755 -D $BOARD_DIR/S60alsa ${TARGET_DIR}/etc/init.d/S60alsa
+install -m 0644 -D $BOARD_DIR/asound.state ${TARGET_DIR}/var/lib/alsa/asound.state
