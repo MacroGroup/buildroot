0c815fe26a2378b2642a8e51926ac088a6d94a69
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-08-30 18:59:46 +0300

    Initial rework board files to make commaon MacroGroup place

--- board/rk3568-mg-evb/post-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-BOARD_DIR="$(dirname $0)"
-
-ln -sr $BOARD_DIR/barebox-rk3568-mg-evb.img $BINARIES_DIR
-
-install -m 0755 -D $BOARD_DIR/S60alsa ${TARGET_DIR}/etc/init.d/S60alsa
-install -m 0644 -D $BOARD_DIR/asound.state ${TARGET_DIR}/var/lib/alsa/asound.state
