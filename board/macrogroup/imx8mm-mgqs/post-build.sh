2a3498abb36c1e6a920f3c7202cb4a10f539f97c
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-09-04 18:33:21 +0300

    Add modetest util to defconfig, update barebox image for imx8

--- board/macrogroup/imx8mm-mgqs/post-build.sh
+++ board/macrogroup/imx8mm-mgqs/post-build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 BOARD_DIR="$(dirname $0)"
 
-ln -sr $BOARD_DIR/Image.gz $BINARIES_DIR
-ln -sr $BOARD_DIR/barebox-nxp-imx8mm-mgqs.img $BINARIES_DIR
+ln -srf $BOARD_DIR/Image.gz $BINARIES_DIR
+ln -srf $BOARD_DIR/barebox-diasom-imx8m-evb.img $BINARIES_DIR
 
 install -m 0755 -D $BOARD_DIR/../common/S60alsa ${TARGET_DIR}/etc/init.d/S60alsa
 install -m 0644 -D $BOARD_DIR/../common/asound.state ${TARGET_DIR}/var/lib/alsa/asound.state
