29fa7792f44b3abd4ae822797213552e0faff4bc
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-09-05 11:12:21 +0300

    Use overlay rootfs directory

--- board/macrogroup/imx8mm-mgqs/post-build.sh
+++ board/macrogroup/imx8mm-mgqs/post-build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
+
 BOARD_DIR="$(dirname $0)"
 
 ln -srf $BOARD_DIR/Image.gz $BINARIES_DIR
-
-install -m 0755 -D $BOARD_DIR/../common/S60alsa ${TARGET_DIR}/etc/init.d/S60alsa
-install -m 0644 -D $BOARD_DIR/../common/asound.state ${TARGET_DIR}/var/lib/alsa/asound.state
