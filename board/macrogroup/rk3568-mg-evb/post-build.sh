d8fef9af5a57decebe488168746b84b43808b4c5
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-09-05 11:12:21 +0300

    Use overlay rootfs directory

--- board/macrogroup/rk3568-mg-evb/post-build.sh
+++ board/macrogroup/rk3568-mg-evb/post-build.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-BOARD_DIR="$(dirname $0)"
 
-install -m 0755 -D $BOARD_DIR/../common/S60alsa ${TARGET_DIR}/etc/init.d/S60alsa
-install -m 0644 -D $BOARD_DIR/../common/asound.state ${TARGET_DIR}/var/lib/alsa/asound.state
+BOARD_DIR="$(dirname $0)"
