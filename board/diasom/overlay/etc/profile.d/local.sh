c8b6f6db56c056a3b97f2760eb55b6b8f511ee14
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-09-15 08:29:12 +0300

    Update local variables

--- board/diasom/overlay/etc/profile.d/local.sh
+++ board/diasom/overlay/etc/profile.d/local.sh
@@ -3,9 +3,14 @@ export INPUTRC='/etc/inputrc'
 export LANG='en_US'
 export LC_ALL='C'
 export PATH=$PATH':/usr/local/bin:/usr/local/sbin'
+export PS1='[\[\e[1;31m\]\u\[\e[m\]@\[\e[1;32m\]\h\[\e[m\] \[\e[1;33m\]\w\[\e[m\]\$ '
 export TERM='vt102'
 export TMPDIR='/var/tmp'
 export TZ='UTC'
+
+[ -z $XDG_RUNTIME_DIR ] && export XDG_RUNTIME_DIR=/run/user/$(id -u)
+
+ulimit -c 0
 
 # Unhide console cursor
 echo -e -n "\e[?25h"
