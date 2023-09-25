e2d0d83c89ec9f9478e1e1ff44a6fd45e014bcb6
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-09-25 16:54:55 +0300

    imx8: Add ESP32 tests

---
+++ board/diasom/overlay/usr/local/bin/esp32_scan.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+DEV=/dev/ttymxc0
+MOD=/tmp/esp32
+
+[ -e $DEV ] || { echo "Script is not supported on this platform!"; exit 1; }
+
+# Ensure no listener on serial port
+killall tail 2&> /dev/null
+
+# Turn Power OFF
+gpioset gpiochip3 31=0
+sleep 1
+
+# Install UART listener
+tail -f $DEV &
+PID=$!
+
+MODE=$(cat $MOD 2&> /dev/null)
+# Check if already downloaded
+if [ "$MODE" != "scan" ]; then
+	# Switch to UART download mode
+	gpioset gpiochip3 28=0
+	sleep 0.1
+
+	# Turn Power ON
+	gpioset gpiochip3 31=1
+	sleep 1.5
+
+	esptool.py -p $DEV --chip esp32 -b 115200 \
+	--before default_reset --after hard_reset \
+	write_flash --flash_mode dio --flash_size 2MB --flash_freq 40m \
+	0x1000 /usr/local/share/esp32/bootloader.bin \
+	0x8000 /usr/local/share/esp32/partition-table.bin \
+	0x10000 /usr/local/share/esp32/scan.bin || exit 1
+
+	# Turn Power OFF
+	gpioset gpiochip3 31=0
+	sleep 1
+
+	echo -n scan > $MOD
+fi
+
+# Switch to SPI download mode
+gpioset gpiochip3 28=1
+sleep 0.1
+
+# Turn Power ON
+gpioset gpiochip3 31=1
+sleep 0.1
+
+# Wait for any key
+while [ true ]; do
+	read -t 1 -n 1
+	if [ $? = 0 ]; then
+		break;
+	fi
+done
+
+# Remove UART listener
+kill $PID 2&> /dev/null
+
+exit 0
