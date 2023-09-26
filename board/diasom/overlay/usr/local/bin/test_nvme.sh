30a90616404af7a7669254e9ed75f3104c857578
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-09-26 16:23:39 +0300

    Typo fix

--- board/diasom/overlay/usr/local/bin/test_nvme.sh
+++ board/diasom/overlay/usr/local/bin/test_nvme.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-NVMEM=/dev/nvme0
+NVME=/dev/nvme0
 
-[ -e $NVMEM ] || { echo "NVMe disk is not found!"; exit 1; }
+[ -e $NVME ] || { echo "NVMe disk is not found!"; exit 1; }
 
-iozone -A -N -q 1024 -g 1024 $NVMEM
+iozone -A -N -q 1024 -g 1024 $NVME
 
 exit 0
