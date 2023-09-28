4506e177333da96bd0ba99d56a3a99c631144c07
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-09-28 11:27:02 +0300

    test_fb: Add check for framebuffer

--- board/diasom/overlay/usr/local/bin/test_fb.sh
+++ board/diasom/overlay/usr/local/bin/test_fb.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+[ -f /sys/class/graphics/fb0 ] || { echo "Script cannot be executed without framebuffer!"; exit 1; }
+
 gst-launch-1.0 \
 videotestsrc ! \
 video/x-raw,width=1920,height=1080 ! \
