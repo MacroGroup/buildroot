2143aee4f5f1fa1e871c105df513e88e7e2123d1
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-10-05 10:23:15 +0300

    Install fastboot link on any build if it exists

--- board/diasom/post-build-ds-rk3568-evb.sh
+++ board/diasom/post-build-ds-rk3568-evb.sh
@@ -4,8 +4,6 @@ BOARD_DIR="$(dirname $0)"
 
 $BOARD_DIR/post-build-common.sh
 
-ln -sf $HOST_DIR/bin/fastboot $BINARIES_DIR/fastboot
-
 install -m 0755 -D $BOARD_DIR/usb-upload-boot-ds-rk3568-evb.sh $BINARIES_DIR/usb-upload-boot.sh
 install -m 0755 -D $BOARD_DIR/usb-upload-emmc-ds-rk3568-evb.sh $BINARIES_DIR/usb-upload-emmc.sh
 
