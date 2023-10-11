968b92579a13f4ce1d527eae88bf5d97ffd815d4
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-10-11 22:19:45 +0300

    Rework test_jpeg test

--- board/diasom/overlay/usr/local/bin/test_jpeg.sh
+++ board/diasom/overlay/usr/local/bin/test_jpeg.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
 if [ ! -d /sys/bus/platform/devices/fdee0000.video-codec ]; then
-	echo "Test JPEG is not supported on this platform!"
+	echo "HW JPEG encoding test is not supported on this platform!"
 	exit 1
 fi
 
+timetest()
+{
+	time gst-launch-1.0 -q --no-position \
+	videotestsrc num-buffers=1000 ! \
+	$1 ! fakesink sync=false
+}
+
 echo "Measure JPEG hardware encoding (1000 frames):"
-time gst-launch-1.0 -q --no-position videotestsrc num-buffers=1000 ! \
-v4l2jpegenc ! fakesink sync=false
+timetest v4l2jpegenc
 
 echo "Measure JPEG software encoding (1000 frames):"
-time gst-launch-1.0 -q --no-position videotestsrc num-buffers=1000 ! \
-jpegenc ! fakesink sync=false
+timetest jpegenc
 
 exit 0
