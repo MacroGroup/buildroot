bfa3b883f0d5d0008df45a1b925eccd6c2fc52c7
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-10-16 10:09:41 +0300

    test_jpeg: Add some checks

--- board/diasom/overlay/usr/local/bin/test_jpeg.sh
+++ board/diasom/overlay/usr/local/bin/test_jpeg.sh
@@ -1,21 +1,38 @@
 #!/bin/sh
 
+for i in gst-inspect-1.0 gst-launch-1.0; do
+	if ! which $i >/dev/null 2>&1; then
+		echo "Script cannot be executed due missing \"$i\" tool!"
+		exit 1
+	fi
+done
+
 if [ ! -d /sys/bus/platform/devices/fdee0000.video-codec ]; then
-	echo "HW JPEG encoding test is not supported on this platform!"
+	echo "This test is not supported on this platform!"
 	exit 1
 fi
 
+plugincheck()
+{
+	gst-inspect-1.0 --exists $1
+	if [[ $? -ne 0 ]]; then
+		echo "Script cannot be executed due missing \"$1\" plugin!"
+		exit 1
+	fi
+}
+
 timetest()
 {
+	echo "Measure JPEG encoding using \"$1\" plugin (1000 frames):"
 	time gst-launch-1.0 -q --no-position \
 	videotestsrc num-buffers=1000 ! \
 	$1 ! fakesink sync=false
 }
 
-echo "Measure JPEG hardware encoding (1000 frames):"
-timetest v4l2jpegenc
+plugincheck v4l2jpegenc
+plugincheck jpegenc
 
-echo "Measure JPEG software encoding (1000 frames):"
+timetest v4l2jpegenc
 timetest jpegenc
 
 exit 0
