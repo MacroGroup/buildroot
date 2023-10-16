8cda8ccc1d37910be20105c9270964d6a2d63dcc
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-10-16 16:32:22 +0300

    Add more SD space. Add RGA test for rk3568

---
+++ board/diasom/overlay/usr/local/bin/test_rga.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+for i in gst-inspect-1.0 gst-launch-1.0; do
+	if ! which $i >/dev/null 2>&1; then
+		echo "Script cannot be executed due missing \"$i\" tool!"
+		exit 1
+	fi
+done
+
+if [ ! -d /sys/bus/platform/devices/fdeb0000.rga ]; then
+	echo "This test is not supported on this platform!"
+	exit 1
+fi
+
+plugincheck()
+{
+	gst-inspect-1.0 --exists $1
+	if [[ $? -ne 0 ]]; then
+		echo "Script cannot be executed due missing \"$1\" plugin!"
+		exit 1
+	fi
+}
+
+timetest()
+{
+	echo "Measure image scaling using \"$1\" plugin (10 frames):"
+	time gst-launch-1.0 -q --no-position \
+	videotestsrc num-buffers=10 ! \
+	video/x-raw,width=1920,height=1080 ! \
+	$1 ! \
+	video/x-raw,width=800,height=600 ! \
+	fakesink sync=false
+}
+
+plugincheck videotestsrc
+plugincheck fakesink
+plugincheck videoscale
+plugincheck v4l2convert
+
+timetest v4l2convert
+timetest videoscale
+
+exit 0
