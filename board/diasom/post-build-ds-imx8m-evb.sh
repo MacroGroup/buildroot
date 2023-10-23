2fe7233f1cca2f984d775e96f5b9573009f13d0c
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-10-23 21:14:31 +0300

    imx8m: Add support for USB boot & update

--- board/diasom/post-build-ds-imx8m-evb.sh
+++ board/diasom/post-build-ds-imx8m-evb.sh
@@ -5,5 +5,6 @@ BOARD_DIR="$(dirname $0)"
 $BOARD_DIR/post-build-common.sh
 
 install -m 0755 -D $BOARD_DIR/usb-upload-boot-ds-imx8m-evb.sh $BINARIES_DIR/usb-upload-boot.sh
+install -m 0755 -D $BOARD_DIR/usb-upload-emmc-ds-imx8-evb.sh $BINARIES_DIR/usb-upload-emmc.sh
 
 exit 0
---
+++ board/diasom/usb-upload-emmc-ds-imx8m-evb.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+ROOT=$(dirname -- $(readlink -f -- "$0"))
+
+if [ ! -f $ROOT/fastboot ]; then
+	echo "\"fastboot\" program is not found!"
+	exit 1
+fi
+
+if [ ! -f $ROOT/ds-imx8m-evb-sdcard.img ]; then
+	echo "\"ds-imx8m-evb-sdcard.img\" image is not found!"
+	exit 1
+fi
+
+echo "This script downloads the full SD card image to the EMMC chip"
+echo "of the development board."
+echo "Make sure the USB cable from the development board to the computer"
+echo "is connected and the development board is in the bootloader state."
+echo "Attention: You will need a superuser password for the script to work!"
+
+read -n 1 -s -p "Press any key to continue..."
+echo
+
+sudo $ROOT/fastboot -i 7531 -S 128M flash emmc $ROOT/ds-imx8m-evb-sdcard.img
+
+exit 0
