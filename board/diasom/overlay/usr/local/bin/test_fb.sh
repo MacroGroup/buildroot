3787677848bb39e68b14e485efaf36bae3b718f3
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-11-27 17:25:32 +0300

    Start Weston at boot

--- board/diasom/overlay/usr/local/bin/test_fb.sh
+++ board/diasom/overlay/usr/local/bin/test_fb.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-[ -d /sys/class/graphics/fb0 ] || { echo "Script cannot be used without framebuffer!"; exit 1; }
+. /etc/profile.d/xdg.sh
+
+if [ ! -d /sys/class/graphics/fb0 ]; then
+	echo "Script cannot be used without framebuffer!"
+	exit 1
+fi
+
+OUTSINK=fbdevsink
+if [ -f $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY.lock ]; then
+	echo "Wayland is active. Image will be dispalyed on the wayland screen!"
+	OUTSINK=waylandsink
+fi
 
 gst-launch-1.0 \
 videotestsrc ! \
 video/x-raw,width=1920,height=1080 ! \
 videoconvert ! \
-fbdevsink
+$OUTSINK
 
 exit 0
