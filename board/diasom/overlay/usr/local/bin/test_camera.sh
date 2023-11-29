002bbb6ac30a6ea9100a84fd391497f45cfa8f29
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-11-29 08:50:31 +0300

    Cleanup camera test script

--- board/diasom/overlay/usr/local/bin/test_camera.sh
+++ board/diasom/overlay/usr/local/bin/test_camera.sh
@@ -1,24 +1,18 @@
 #!/bin/sh
 
-if [ -f $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY.lock ]; then
-	OUTSINK=waylandsink
-	echo "Wayland is active. Image will be dispalyed on the wayland screen!"
-else
-	OUTSINK=fbdevsink
-	if [ ! -d /sys/class/graphics/fb0 ]; then
-		echo "Script cannot be used without framebuffer!"
-		exit 1
-	fi
+if [ ! -f $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY.lock ]; then
+	echo "Script works on Wayland screen only!"
+	exit 1
 fi
 
 # TODO:
-#gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw,format=UYVY,width=1920,height=1080 ! queue ! glupload ! glcolorconvert ! gldownload
-#! video/x-raw,format=RGB16 ! waylandsink
-
-#gst-launch-1.0 \
-#videotestsrc ! \
-#video/x-raw,width=1920,height=1080 ! \
-#videoconvert ! \
-#$OUTSINK
+# gst-launch-1.0 \
+# v4l2src device=/dev/video0 ! \
+# video/x-raw,format=UYVY,width=1920,height=1080 ! \
+# queue ! \
+# glupload ! \
+# glcolorconvert ! \
+# gldownload video/x-raw,format=RGB16 ! \
+# waylandsink
 
 exit 0
