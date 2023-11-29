ff50d778b4138f34e5a833c24b7c00a8d62b5a85
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-11-29 09:00:39 +0300

    Add tests and finalize camera test script

--- board/diasom/overlay/usr/local/bin/test_camera.sh
+++ board/diasom/overlay/usr/local/bin/test_camera.sh
@@ -5,14 +5,35 @@ if [ ! -f $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY.lock ]; then
 	exit 1
 fi
 
-# TODO:
-# gst-launch-1.0 \
-# v4l2src device=/dev/video0 ! \
-# video/x-raw,format=UYVY,width=1920,height=1080 ! \
-# queue ! \
-# glupload ! \
-# glcolorconvert ! \
-# gldownload video/x-raw,format=RGB16 ! \
-# waylandsink
+for i in gst-inspect-1.0 gst-launch-1.0; do
+	if ! which $i >/dev/null 2>&1; then
+		echo "Script cannot be executed due missing \"$i\" tool!"
+		exit 1
+	fi
+done
+
+plugincheck()
+{
+	gst-inspect-1.0 --exists $1
+	if [[ $? -ne 0 ]]; then
+		echo "Script cannot be executed due missing \"$1\" plugin!"
+		exit 1
+	fi
+}
+
+plugincheck v4l2src
+plugincheck glupload
+plugincheck glcolorconvert
+plugincheck gldownload
+plugincheck waylandsink
+
+gst-launch-1.0 \
+v4l2src device=/dev/video0 ! \
+video/x-raw,format=UYVY,width=1920,height=1080 ! \
+queue ! \
+glupload ! \
+glcolorconvert ! \
+gldownload video/x-raw,format=RGB16 ! \
+waylandsink
 
 exit 0
--- board/diasom/overlay/usr/local/bin/test_fb.sh
+++ board/diasom/overlay/usr/local/bin/test_fb.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
-[ -f /etc/profile.d/xdg.sh ] && . /etc/profile.d/xdg.sh
+for i in gst-inspect-1.0 gst-launch-1.0; do
+	if ! which $i >/dev/null 2>&1; then
+		echo "Script cannot be executed due missing \"$i\" tool!"
+		exit 1
+	fi
+done
+
+plugincheck()
+{
+	gst-inspect-1.0 --exists $1
+	if [[ $? -ne 0 ]]; then
+		echo "Script cannot be executed due missing \"$1\" plugin!"
+		exit 1
+	fi
+}
 
 if [ -f $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY.lock ]; then
 	OUTSINK=waylandsink
@@ -12,6 +26,10 @@ else
 		exit 1
 	fi
 fi
+
+plugincheck videotestsrc
+plugincheck videoconvert
+plugincheck $OUTSINK
 
 gst-launch-1.0 \
 videotestsrc ! \
