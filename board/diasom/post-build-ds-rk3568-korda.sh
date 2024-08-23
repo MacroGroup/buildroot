2dd06da8b1215768c546c77bf698078bf152778c
alex <skupov93@gmail.com>
2024-08-23 12:04:37 +0500

    Added configs for new board.

---
+++ board/diasom/post-build-ds-rk3568-korda.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+BOARD_DIR="$(dirname $0)"
+
+$BOARD_DIR/post-build-common.sh
+
+install -m 0755 -D $BOARD_DIR/usb-upload-boot-ds-rk3568-korda.sh $BINARIES_DIR/usb-upload-boot.sh
+install -m 0755 -D $BOARD_DIR/usb-upload-emmc-ds-rk3568-korda.sh $BINARIES_DIR/usb-upload-emmc.sh
+
+exit 0
