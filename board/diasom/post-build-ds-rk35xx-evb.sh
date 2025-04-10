b2ee8ee246f28fed459e4ee81d2833791e78c67e
Alexander Shiyan <eagle.alexander923@gmail.com>
2025-04-10 16:54:17 +0300

    rk35xx: Add fake image generation for RK3568-SMARC

--- board/diasom/post-build-ds-rk35xx-evb.sh
+++ board/diasom/post-build-ds-rk35xx-evb.sh
@@ -7,4 +7,9 @@ $BOARD_DIR/post-build-common.sh
 install -m 0755 -D $BOARD_DIR/usb-upload-boot-ds-rk35*-evb.sh $BINARIES_DIR
 install -m 0755 -D $BOARD_DIR/usb-upload-emmc-ds-rk35*-evb.sh $BINARIES_DIR
 
+if [ ! -f $BINARIES_DIR/barebox-diasom-rk3568-som-smarc-evb.img ]; then
+	cd $BINARIES_DIR
+	ln -sf barebox-diasom-rk3568-som-evb.img barebox-diasom-rk3568-som-smarc-evb.img
+fi
+
 exit 0
