651c874554bc3c0e105a801be697686b9955c0fa
Alexander Shiyan <eagle.alexander923@gmail.com>
2025-06-10 20:48:26 +0300

    post-build-common.sh: Improve script

--- board/diasom/post-build-common.sh
+++ board/diasom/post-build-common.sh
@@ -1,12 +1,32 @@
 #!/bin/sh
 
-# Install compiled device tree overlays
-for overlay in $BINARIES_DIR/*.dtbo; do
-	[ -e "$overlay" ] || continue
-	install -m 0644 -D $overlay $TARGET_DIR/boot
+# Exit immediately on any error
+set -e
+
+# Verify required environment variables are set
+for var in BINARIES_DIR TARGET_DIR HOST_DIR; do
+	eval "value=\"\${$var}\""
+	if [ -z "$value" ]; then
+		echo "Error: $var is not set" >&2
+		exit 1
+	fi
 done
 
+# Install compiled device tree overlays
+if [ -d "$BINARIES_DIR" ]; then
+	for overlay in "$BINARIES_DIR"/*.dtbo; do
+		[ -f "$overlay" ] || continue
+		filename="${overlay##*/}"
+		install -m 0644 -D "$overlay" "${TARGET_DIR}/boot/${filename}"
+	done
+else
+	echo "Warning: BINARIES_DIR '$BINARIES_DIR' does not exist" >&2
+fi
+
 # Install fastboot link
-[ -f $HOST_DIR/bin/fastboot ] && ln -sf $HOST_DIR/bin/fastboot $BINARIES_DIR/fastboot
+if [ -f "${HOST_DIR}/bin/fastboot" ]; then
+	mkdir -p "$BINARIES_DIR"
+	ln -sf "${HOST_DIR}/bin/fastboot" "${BINARIES_DIR}/fastboot"
+fi
 
 exit 0
