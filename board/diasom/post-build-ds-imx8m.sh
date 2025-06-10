fef72487ec941fda92b4d56cd9df9c99bab93251
Alexander Shiyan <eagle.alexander923@gmail.com>
2025-06-10 21:35:31 +0300

    diasom: Improve post-build scripts

--- board/diasom/post-build-ds-imx8m.sh
+++ board/diasom/post-build-ds-imx8m.sh
@@ -1,10 +1,32 @@
 #!/bin/sh
 
-BOARD_DIR="$(dirname $0)"
+set -e
 
-$BOARD_DIR/post-build-common.sh
+BOARD_DIR="$(dirname "$0")"
+if [ ! -d "${BOARD_DIR}" ]; then
+	echo "Error: BOARD_DIR '${BOARD_DIR}' does not exist" >&2
+	exit 1
+fi
 
-install -m 0755 -D $BOARD_DIR/usb-upload-boot-ds-imx8m-evb.sh $BINARIES_DIR/usb-upload-boot.sh
-install -m 0755 -D $BOARD_DIR/usb-upload-emmc-ds-imx8m-evb.sh $BINARIES_DIR/usb-upload-emmc.sh
+"${BOARD_DIR}/post-build-common.sh"
+
+install_renamed_script() {
+	local src="$1"
+	local dest_name="$2"
+
+	if [ ! -f "${src}" ]; then
+		echo "Error: Source script ${src} not found" >&2
+		exit 1
+	fi
+
+	install -v -m 0755 "${src}" "${BINARIES_DIR}/${dest_name}"
+}
+
+install_renamed_script \
+	"${BOARD_DIR}/usb-upload-boot-ds-imx8m-evb.sh" \
+	"usb-upload-boot.sh"
+install_renamed_script \
+	"${BOARD_DIR}/usb-upload-emmc-ds-imx8m-evb.sh" \
+	"usb-upload-emmc.sh"
 
 exit 0
--- board/diasom/post-build-ds-rk35xx-evb.sh
+++ board/diasom/post-build-ds-rk35xx-evb.sh
@@ -1,10 +1,32 @@
 #!/bin/sh
 
-BOARD_DIR="$(dirname $0)"
+set -e
 
-$BOARD_DIR/post-build-common.sh
+BOARD_DIR="$(dirname "$0")"
+if [ ! -d "${BOARD_DIR}" ]; then
+	echo "Error: BOARD_DIR ${BOARD_DIR} does not exist" >&2
+	exit 1
+fi
 
-install -m 0755 -D $BOARD_DIR/usb-upload-boot-ds-rk35*-evb.sh $BINARIES_DIR
-install -m 0755 -D $BOARD_DIR/usb-upload-emmc-ds-rk35*-evb.sh $BINARIES_DIR
+"${BOARD_DIR}/post-build-common.sh"
+
+install_scripts() {
+	local pattern="$1"
+	local found=0
+
+	for script in "${BOARD_DIR}"/${pattern}; do
+		if [ -f "${script}" ]; then
+			install -v -m 0755 "${script}" "${BINARIES_DIR}"
+			found=1
+		fi
+	done
+
+	if [ "${found}" -eq 0 ]; then
+		echo "Warning: No files matching pattern '${pattern}' found in ${BOARD_DIR}" >&2
+	fi
+}
+
+install_scripts "usb-upload-boot-ds-rk35*-evb.sh"
+install_scripts "usb-upload-emmc-ds-rk35*-evb.sh"
 
 exit 0
