4fa43fefb4fb20fcb5df9fe5a78ca5c63e3b8f24
Alexander Shiyan <eagle.alexander923@gmail.com>
2025-07-01 20:28:46 +0300

    diasom: Extend test_nvme script

--- board/diasom/overlay/usr/local/bin/test_nvme.sh
+++ board/diasom/overlay/usr/local/bin/test_nvme.sh
@@ -4,6 +4,14 @@ NVME=/dev/nvme0
 
 [ -e $NVME ] || { echo "NVMe disk is not found!"; exit 1; }
 
-iozone -A -N -q 1024 -g 1024 $NVME || exit 1
+NVMEPART=/dev/nvme0n1
+
+if [ -e $NVMEPART ]; then
+	fio --name=test --filename=$NVMEPART --ioengine=libaio \
+		--rw=randrw --bs=4k --numjobs=1 --size=1G \
+		--runtime=60 --time_based --direct=1 || exit 1
+else
+	iozone -A -N -q 1024 -g 1024 $NVME || exit 1
+fi
 
 exit 0
