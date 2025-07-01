df92795f0997b1e18dd862396f43d9a80c87fd2d
Alexander Shiyan <eagle.alexander923@gmail.com>
2025-07-01 21:49:15 +0300

    diasom: test_nvme: More improve tests

--- board/diasom/overlay/usr/local/bin/test_nvme.sh
+++ board/diasom/overlay/usr/local/bin/test_nvme.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 NVME=/dev/nvme0
 
 [ -e $NVME ] || { echo "NVMe disk is not found!"; exit 1; }
@@ -8,10 +10,10 @@ NVMEPART=/dev/nvme0n1
 
 if [ -e $NVMEPART ]; then
 	fio --name=test --filename=$NVMEPART --ioengine=libaio --rw=randrw \
-		--bs=4k --numjobs=1 --size=1G --runtime=60 --time_based \
-		--direct=1 --eta=always --iodepth=32 || exit 1
+		--bs=4k --numjobs=$(nproc) --size=1G --runtime=60 \
+		--time_based --direct=1 --eta=always --iodepth=32 || exit 1
 else
-	iozone -A -N -q 1024 -g 1024 $NVME || exit 1
+	iozone -A -I -N -q 1024 -g 1024 $NVME || exit 1
 fi
 
 exit 0
