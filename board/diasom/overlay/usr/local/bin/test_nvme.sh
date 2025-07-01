8938ef792afc86ecf602622682e97143003e07de
Alexander Shiyan <eagle.alexander923@gmail.com>
2025-07-01 21:36:05 +0300

    diasom: test_nvme: Improve fio test

--- board/diasom/overlay/usr/local/bin/test_nvme.sh
+++ board/diasom/overlay/usr/local/bin/test_nvme.sh
@@ -7,9 +7,9 @@ NVME=/dev/nvme0
 NVMEPART=/dev/nvme0n1
 
 if [ -e $NVMEPART ]; then
-	fio --name=test --filename=$NVMEPART --ioengine=libaio \
-		--rw=randrw --bs=4k --numjobs=1 --size=1G \
-		--runtime=60 --time_based --direct=1 || exit 1
+	fio --name=test --filename=$NVMEPART --ioengine=libaio --rw=randrw \
+		--bs=4k --numjobs=1 --size=1G --runtime=60 --time_based \
+		--direct=1 --eta=always --iodepth=32 || exit 1
 else
 	iozone -A -N -q 1024 -g 1024 $NVME || exit 1
 fi
