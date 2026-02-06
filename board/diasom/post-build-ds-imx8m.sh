d84f383cda4fe884759c65d46c03fc6df78b750a
Alexander Shiyan <eagle.alexander923@gmail.com>
2026-02-06 10:24:06 +0300

    diasom: imx8m: Switch to wildcard image generation

--- board/diasom/post-build-ds-imx8m.sh
+++ board/diasom/post-build-ds-imx8m.sh
@@ -7,7 +7,7 @@ if [ ! -d "${BOARD_DIR}" ]; then
 	echo "Error: BOARD_DIR ${BOARD_DIR} does not exist" >&2
 	exit 1
 fi
-BOARD_NAME="${2:-ds-imx8m}"
+BOARD_NAME="${2:-ds-imx8m*}"
 
 source "${BOARD_DIR}/post-build-common.sh"
 
