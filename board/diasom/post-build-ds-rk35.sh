b06a667b6f1a961725b7af3a8cec3016d540cc8e
Alexander Shiyan <eagle.alexander923@gmail.com>
2026-02-06 10:47:23 +0300

    diasom: rk35xx: Switch to wildcard image generation

--- board/diasom/post-build-ds-rk35.sh
+++ board/diasom/post-build-ds-rk35.sh
@@ -7,7 +7,7 @@ if [ ! -d "${BOARD_DIR}" ]; then
 	echo "Error: BOARD_DIR ${BOARD_DIR} does not exist" >&2
 	exit 1
 fi
-BOARD_NAME="${2:-ds-rk35}"
+BOARD_NAME="${2:-ds-rk35*}"
 
 source "${BOARD_DIR}/post-build-common.sh"
 
