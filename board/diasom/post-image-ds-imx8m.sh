8850fa378f364f8f78b948c3338659c5d95fef02
Alexander Shiyan <eagle.alexander923@gmail.com>
2026-02-06 07:46:50 +0300

    diasom: Use post-image script to run genimage

---
+++ board/diasom/post-image-ds-imx8m.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+BOARD_DIR="$(dirname "$0")"
+if [ ! -d "${BOARD_DIR}" ]; then
+	echo "Error: BOARD_DIR ${BOARD_DIR} does not exist" >&2
+	exit 1
+fi
+
+genimage_type() {
+	echo "genimage-ds-imx8m.cfg"
+}
+
+support/scripts/genimage.sh -c "${BOARD_DIR}/$(genimage_type)"
+
+exit 0
